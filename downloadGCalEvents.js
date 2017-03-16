54de686ea542cafd2d536fd0a76fd17178f4e08b
Austin Miles <abm@austinbmiles.com>
2017-03-16 10:23:55 -0400

    successfully connected to Gcal, added mssql dependency

--- downloadGCalEvents.js
+++ downloadGCalEvents.js
@@ -2,13 +2,14 @@ var fs = require('fs');
 var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
+//var calID = "austinbmiles.com_isc8j69dhqij1sncqefgd9b02o@group.calendar.google.com";
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/calendar-nodejs-quickstart.json
 var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
 	process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
+var TOKEN_PATH = TOKEN_DIR + 'downloadGCalEvents-nodejs.json';
 
 // Load client secrets from a local file.
 fs.readFile('client_secret.json', function processClientSecrets(err, content) {
@@ -126,4 +127,9 @@ function listEvents(auth) {
 			}
 		}
 	});
+}
+
+// Posts Data to Specified MSSQL DB
+function postEventsToDB(auth) {
+
 }
